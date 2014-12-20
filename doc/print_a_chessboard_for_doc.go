115178cf9fac837dea949c3d0f3f0f4fe4b02888
Uwe Janssen <mail@uwe-janssen.de>
2014-12-20 11:44:43 +0100

    printSqares

--- doc/print_a_chessboard_for_doc.go
+++ doc/print_a_chessboard_for_doc.go
@@ -71,6 +71,18 @@ const (
 	h8 = 63
 )
 
+func printSqares() string {
+	files := "abcdefgh"
+	s := ""
+	for rank := 8; rank > 0; rank-- {
+		for file := 0; file < len(files); file++ {
+			s += fmt.Sprintf("%s%d ", string(files[file]), rank)
+		}
+		s += fmt.Sprintf("\n")
+	}
+	return s
+}
+
 func printBoard() string {
 	files := "a  b  c  d  e  f  g  h "
 	firstSquares := [...]int{a8, a7, a6, a5, a4, a3, a2, a1}
@@ -79,7 +91,7 @@ func printBoard() string {
 	for _, r := range firstSquares {
 		s += fmt.Sprintf("%d ", rank)
 		for f := 0; f < 8; f++ {
-			s += fmt.Sprintf("%02d ", r+f)
+			s += fmt.Sprintf("%2d ", r+f)
 		}
 		s += fmt.Sprintf("%d\n", rank)
 		rank--
@@ -89,6 +101,8 @@ func printBoard() string {
 }
 func main() {
 	fmt.Printf("A chess board\n")
+	fmt.Printf("\n")
+	fmt.Printf("%s\n", printSqares())
 	fmt.Printf("\n")
 	fmt.Printf("%s\n", printBoard())
 }
