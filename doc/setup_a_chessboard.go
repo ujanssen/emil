6a70a9c1c7070b400df4e3d9d61fe3f0f402453c
Uwe Janssen <mail@uwe-janssen.de>
2014-12-21 18:30:12 +0100

    add indexes.go

--- doc/setup_a_chessboard.go
+++ doc/setup_a_chessboard.go
@@ -10,11 +10,9 @@ func main() {
 	board := emil.NewBoard()
 	board.Setup(emil.WhiteKing, emil.F6)
 	board.Setup(emil.BlackKing, emil.G8)
-	board.Setup(emil.WhiteRock, emil.B1)
+	board.Setup(emil.WhiteRock, emil.B8)
 	fmt.Printf("\n")
 	fmt.Printf("%s\n", board)
 
-	fmt.Printf("K F6: %s\n", board.Moves(emil.WhiteKing, emil.F6))
-	fmt.Printf("R B1: %s\n", board.Moves(emil.WhiteRock, emil.B1))
 	fmt.Printf("k G8: %s\n", board.Moves(emil.BlackKing, emil.G8))
 }
