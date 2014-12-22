40bf6d65a27c3c7554d7c7beb260de003c55f19d
Uwe Janssen <mail@uwe-janssen.de>
2014-12-22 08:18:32 +0100

    add init.go

--- board.go
+++ board.go
@@ -71,28 +71,3 @@ func (b *Board) Moves(player int) string {
 	}
 	return s
 }
-
-func init() {
-	// define BoardSquares
-	for j := 0; j < 8; j++ {
-		for i, file := range FILES {
-			index := i + j*8
-			BoardSquares[index] = newSquare(string(file), j+1, index)
-		}
-	}
-	// define squaresDistances
-	for _, s := range BoardSquares {
-		for _, r := range FirstSquares {
-			for f := 0; f < 8; f++ {
-				squaresDistances[s.index][r+f] = s.distance(BoardSquares[r+f])
-			}
-		}
-	}
-
-	// compute piece moves
-	for i := A1; i <= H8; i++ {
-		kingMoves[i] = kingDestinationsFrom(i)
-		rockMoves[i] = rockDestinationsFrom(i)
-	}
-
-}
