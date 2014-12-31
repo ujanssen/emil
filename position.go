451e8df039c6e850ca1bc9a61d02f8d8ce9985da
Uwe Janssen <mail@uwe-janssen.de>
2014-12-31 10:28:31 +0100

    print position FEN like

--- position.go
+++ position.go
@@ -1,5 +1,10 @@
 package emil
 
+import (
+	"fmt"
+	"strings"
+)
+
 type position struct {
 	board  *Board
 	player int
@@ -9,4 +14,25 @@ func NewPosition(board *Board, player int) *position {
 	return &position{
 		board:  board,
 		player: player}
+}
+
+func (p *position) String() string {
+	s := ""
+	for _, r := range FirstSquares {
+		if len(s) > 0 {
+			s += "/"
+		}
+		for f := 0; f < 8; f++ {
+			s += fmt.Sprintf("%s", symbol(p.board.squares[r+f]))
+		}
+	}
+	s = strings.Replace(s, "        ", "8", -1)
+	s = strings.Replace(s, "       ", "7", -1)
+	s = strings.Replace(s, "      ", "6", -1)
+	s = strings.Replace(s, "     ", "5", -1)
+	s = strings.Replace(s, "    ", "4", -1)
+	s = strings.Replace(s, "   ", "3", -1)
+	s = strings.Replace(s, "  ", "2", -1)
+	s = strings.Replace(s, " ", "1", -1)
+	return s
 }
---
+++ position_test.go
@@ -0,0 +1,53 @@
+package emil_test
+
+import (
+	"github.com/ujanssen/emil"
+	"testing"
+)
+
+/*
+  a b c d e f g h
+8                 8
+7                 7
+6                 6
+5                 5
+4                 4
+3                 3
+2                 2
+1                 1
+  a b c d e f g h
+*/
+func TestEmptyBoard(t *testing.T) {
+	board := emil.NewBoard()
+
+	want := "8/8/8/8/8/8/8/8"
+	got := emil.NewPosition(board, emil.WHITE).String()
+	if got != want {
+		t.Errorf("the move should be %s, got %s", want, got)
+	}
+}
+
+/*
+  a b c d e f g h
+8         k       8
+7                 7
+6         K       6
+5                 5
+4                 4
+3                 3
+2                 2
+1               R 1
+  a b c d e f g h
+*/
+func TestFindMoveRh1h8(t *testing.T) {
+	board := emil.NewBoard()
+	board.Setup(emil.WhiteKing, emil.E6)
+	board.Setup(emil.BlackKing, emil.E8)
+	board.Setup(emil.WhiteRock, emil.H1)
+
+	want := "4k3/8/4K3/8/8/8/8/7R"
+	got := emil.NewPosition(board, emil.WHITE).String()
+	if got != want {
+		t.Errorf("the move should be %s, got %s", want, got)
+	}
+}
