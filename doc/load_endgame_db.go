f2f5821370ea345d0b0696fee47b7bb11d59d6de
Uwe Janssen <mail@uwe-janssen.de>
2015-01-01 11:33:46 +0100

    add load_endgame_db.go

---
+++ doc/load_endgame_db.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ujanssen/emil"
+	"time"
+)
+
+func main() {
+	emil.IN_TEST = !true
+	emil.DEBUG = true
+
+	start := time.Now()
+	db, err := emil.LoadEndGameDb()
+	end := time.Now()
+	fmt.Printf("\n\n\nload duration %v\nerr %v\n\n", end.Sub(start), err)
+
+	if err != nil {
+
+		fmt.Printf("len(db.AnalysisMap) %v\n\n", len(db.AnalysisMap))
+
+		board := emil.NewBoard()
+		board.Setup(emil.WhiteKing, emil.E6)
+		board.Setup(emil.BlackKing, emil.E8)
+		board.Setup(emil.WhiteRock, emil.H1)
+
+		want := "Rh1h8"
+		move := db.Find(emil.NewPosition(board, emil.WHITE))
+		if move == nil {
+			fmt.Printf("the move should be %s, got nil", want)
+		} else {
+			got := move.String()
+			if got != want {
+				fmt.Printf("the move should be %s, got %s", want, got)
+			}
+		}
+	}
+}
