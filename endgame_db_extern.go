1aeaa0e804fe5a22d50a0fa601a02d8791d47a7c
Uwe Janssen <mail@uwe-janssen.de>
2015-01-03 08:26:11 +0100

    extract extern endgame db parts

---
+++ endgame_db_extern.go
@@ -0,0 +1,182 @@
+package emil
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
+const filename = "EndGameDb.json"
+
+var IN_TEST = false
+
+type EndGameSave struct {
+	AnalysisMap map[string]string `json:"analysis"`
+}
+
+// EndGameDb to query for mate in 1,2, etc.
+type EndGameDb struct {
+	Start       time.Time            `json:"startTime"`
+	Duration    time.Duration        `json:"duration"`
+	AnalysisMap map[string]*Analysis `json:"analysis"`
+	dtmDb       []map[string]bool
+}
+
+func (db *EndGameDb) Find(p *position) (bestMove *Move) {
+	if DEBUG {
+		fmt.Printf("Find:\n%s\n", p.board)
+	}
+	a := db.AnalysisMap[p.board.String()]
+	if DEBUG {
+		fmt.Printf("Found: AnalysisMap with dtms %v\n", a.DTMs(p.player))
+	}
+	return a.BestMove(p.player)
+}
+
+func (db *EndGameDb) FindMatesIn(dtm int) (as []*Analysis) {
+	if dtm == -1 {
+		for _, a := range db.AnalysisMap {
+			if a.playerHaveDTMs() {
+				as = append(as, a)
+			}
+		}
+	} else {
+		for str := range db.dtmDb[dtm] {
+			as = append(as, db.AnalysisMap[str])
+		}
+	}
+	return as
+}
+
+func (db *EndGameDb) FindMates() (as []*Analysis) {
+	return db.FindMatesIn(0)
+}
+
+func (db *EndGameDb) FindMate(piece, square int) (boards []*Board) {
+	for str := range db.dtmDb[0] {
+		a := db.AnalysisMap[str]
+		if a.board.squares[square] == piece {
+			boards = append(boards, a.board)
+		}
+	}
+	return boards
+}
+
+func (db *EndGameDb) MaxDtm() int {
+	return len(db.dtmDb)
+}
+
+func GenerateMoves(p *position) (list []*Move) {
+	for _, m := range generateMoves(p) {
+		b := p.board.DoMove(m)
+		if !IsTheKingInCheck(NewPosition(b, WHITE)) {
+			list = append(list, m)
+		}
+	}
+	return list
+}
+func LoadEndGameDb() (db *EndGameDb, err error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return db, err
+	}
+	var data EndGameSave
+	err = json.Unmarshal(b, &data)
+	if err != nil {
+		return db, err
+	}
+	db = &EndGameDb{
+		Start:       time.Now(),
+		AnalysisMap: make(map[string]*Analysis),
+		dtmDb:       make([]map[string]bool, 0)}
+
+	for i := 0; i < 34; i++ {
+		db.dtmDb = append(db.dtmDb, make(map[string]bool))
+	}
+
+	for fen, v := range data.AnalysisMap {
+		board := Fen2Board(fen)
+		db.addPosition(board)
+		dtms := DTMsFromString(v)
+		for _, d := range dtms {
+			db.addAnalysis(board, d.dtm, d.move.reverse())
+		}
+	}
+
+	return db, err
+}
+
+// SaveEndGameDb saves the an end game DB for KRK to file
+func SaveEndGameDb(db *EndGameDb) error {
+	fmt.Println("WriteDataToFile: ", filename)
+
+	data := EndGameSave{AnalysisMap: make(map[string]string)}
+
+	for p, a := range db.AnalysisMap {
+		data.AnalysisMap[p] = fmt.Sprintf("%v", a.dtmWhite)
+	}
+
+	b, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, b, 0666)
+}
+
+// NewEndGameDb generates an end game DB for KRK
+func NewEndGameDb() *EndGameDb {
+	var err error
+
+	endGames := &EndGameDb{
+		Start:       time.Now(),
+		AnalysisMap: make(map[string]*Analysis),
+		dtmDb:       make([]map[string]bool, 0)}
+
+	for wk := A1; wk <= H8; wk++ {
+		//for wk := E3; wk <= E3; wk++ {
+		if DEBUG {
+			fmt.Printf("White king on %s\n", BoardSquares[wk])
+		}
+		for wr := A1; wr <= H8; wr++ {
+			for bk := A1; bk <= H8; bk++ {
+
+				board := NewBoard()
+
+				err = board.Setup(WhiteKing, wk)
+				if err != nil {
+					continue
+				}
+
+				err = board.Setup(WhiteRock, wr)
+				if err != nil {
+					continue
+				}
+
+				err = board.Setup(BlackKing, bk)
+				if err != nil {
+					continue
+				}
+
+				err = board.kingsToClose()
+				if err != nil {
+					continue
+				}
+
+				endGames.addPosition(board)
+			}
+		}
+	}
+	end := time.Now()
+	endGames.Duration = end.Sub(endGames.Start)
+	if DEBUG {
+		fmt.Printf("all positions %d\n", 64*63*62)
+		fmt.Printf("endGames.positions() %d\n", endGames.positions())
+		fmt.Printf("difference %d\n", 64*63*62-endGames.positions())
+		fmt.Printf("duration %v\n", endGames.Duration)
+	}
+	endGames.retrogradeAnalysis()
+
+	return endGames
+}
